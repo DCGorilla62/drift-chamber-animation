afb80ec655abf7f0e4702bfe264fca4b8d4eb4e1
Dennis H. Calderon <dennis.henry.calderon@gmail.com>
2022-08-02 08:58:23 -0400

    Updated stuff

--- project/scene.py
+++ project/scene.py
@@ -108,4 +108,6 @@ class ShowScreenResolution(Scene):
         d1 = Line(frame_width * LEFT / 2, frame_width * RIGHT/ 2).to_edge(DOWN)
         self.add(d1)
         self.add(Text(str(pixel_width)).next_to(d1, UP))
-        
+        d2 = Line(frame_height * UP / 2, frame_height * DOWN / 2).to_edge(LEFT)
+        self.add(d2)
+        self.add(Text(str(pixel_height)).next_to(d2, RIGHT))
